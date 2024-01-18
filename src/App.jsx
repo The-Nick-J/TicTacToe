2bf2c5bfbcce682a92cc72c1d91024a16de09eb9
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2024-01-18 13:17:48 +0000

    container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import TicTacToe from './TicTacToe';
 import Tablero from './Tablero';
+import "./App.css";
 
 
 const App = () => {
     return (
-        <>
+        <div className='container'>
         <h1>Tic Tac Toe</h1>
-        </>
+        </div>
     )
 }
 
