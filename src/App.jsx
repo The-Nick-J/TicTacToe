c4fdae4a32c6cc4a98510582c3598641b2f387e6
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2024-01-18 17:38:03 +0000

    nuevo comienzo

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
-import React, { useState } from 'react';
-import Info from './Info';
-import Board from './Board';
+import React from 'react';
 import "./App.css";
+import TicTacToe from './components/TicTacToe';
 
-
-const App = () => {
-
-     // reiniciar el juego
-     const [reset, setReset] = useState(false);
-
-     //actualiza y muestra el ganador
-     const [winner, setWinner] = useState('');
-
-     //actualiza setReset a true
-     const resetBoard = () => {
-         setReset(true);
-     };
-
-
-
-        return (
-            <div className="App">
-            {/*oculta el texto si no hay ganador*/}
-            <div className={`winner ${winner !== "true" ? "false" : "ocultar"}`}>
-            {/*muestra el ganador*/}
-            <div className="winnner-text">{winner}</div>
-            {/*boton para reiniciar el juego*/}
-            <button onClick={() => resetBoard()}>
-                Reset Board
-            </button>
-            </div>
-            {/*muestra el componente tablero*/}
-            <Board
-                reset={reset}
-                setReset={setReset}
-                winner={winner}
-                setWinner={setWinner}
-
-                />
-                <Info />
-            </div>
-
-    );
+function App() {
+    return (
+        <TicTacToe />
+    )
 }
 
 export default App;
---
+++ src/components/Strike.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Strike() {
+  return ( <div></div>
+  );
+}
+
+export default Strike;
