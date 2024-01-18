300a80cd9b6821e575d6445acecde98ca6d7fffc
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2024-01-18 16:20:46 +0000

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import TicTacToe from './TicTacToe';
-import Tablero from './Tablero';
+import React, { useState } from 'react';
+import Info from './Info';
+import Board from './Board';
 import "./App.css";
 
 
 const App = () => {
-    return (
-        <div className='container'>
-        <h1>Tic Tac Toe</h1>
-        </div>
-    )
+
+     // reiniciar el juego
+     const [reset, setReset] = useState(false);
+
+     //actualiza y muestra el ganador
+     const [winner, setWinner] = useState('');
+
+     //actualiza setReset a true
+     const resetBoard = () => {
+         setReset(true);
+     };
+
+
+
+        return (
+            <div className="App">
+            {/*oculta el texto si no hay ganador*/}
+            <div className={`winner ${winner !== "true" ? "false" : "ocultar"}`}>
+            {/*muestra el ganador*/}
+            <div className="winnner-text">{winner}</div>
+            {/*boton para reiniciar el juego*/}
+            <button onClick={() => resetBoard()}>
+                Reset Board
+            </button>
+            </div>
+            {/*muestra el componente tablero*/}
+            <Board
+                reset={reset}
+                setReset={setReset}
+                winner={winner}
+                setWinner={setWinner}
+
+                />
+                <Info />
+            </div>
+
+    );
 }
 
 export default App;
--- src/TicTacToe.jsx
+++
@@ -1,7 +0,0 @@
-const TicTacToe = () => {
-
-}
-
-
-
-export default TicTacToe;
